b052739a7d633011920f8b8a8084f4c33e6d695c
Bene-volent <benevolent733@gmail.com>
2023-08-15 18:02:10 +0530

    Complted

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form')
+const submitButton = document.querySelector(".form__button")
+
+form.sub.addEventListener("invalid", (even) => {
+    event.preventDefault()
+})
+
+submitButton.addEventListener("mouseenter",()=>{
+    if (!form.sub.validity.valid){
+        submitButton.style.setProperty("cursor","not-allowed")
+    }
+    else{
+        submitButton.style.setProperty("cursor","pointer")
+    }
+})
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const email = form.sub.value
+    const data = { email }
+    // console.log(data)
+    alert("Thank you for subscribing!\n We will contact you soon.")
+})
